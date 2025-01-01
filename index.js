0481a83836cdf9364c6a4f84519ad90bff4a7965
MD MAHABUB ALAM <mahabubx7@gmail.com>
2025-01-01 08:40:39 +0000

    chore: final submission

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const req = require('express/lib/request');
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+// setup multer 
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), async (req, res) => {
+  const file = req.file;
+  if (!file) return res.status(400).json({ error: 'No file uploaded' });
+
+  return res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size,
+  });
 });
 
 
